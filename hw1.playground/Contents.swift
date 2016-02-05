6a939efd39e661a441b1969f37015df1a93bd788
Elton Lou <eltonlou@airbears2-10-142-8-138.airbears2.1918.berkeley.edu>
2016-02-04 19:36:33 -0800

    final

--- hw1.playground/Contents.swift
+++ hw1.playground/Contents.swift
@@ -22,33 +22,35 @@ class Words {
 //: ### variables the same type? If not, why?
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: [They are not the same type because the values passed into the constructor can be nil because they are optionals and the instance variables unwrap the optional variables being passed in; and nil is not type String.]
 
 
 //: ## Q2: Variable Types and Function Types
-    func arePalindromes(words: [String]) -> Bool {
+    class func arePalindromes(words: [String]) -> Bool {
         let reversedWords = words.map() {String($0.characters.reverse())}
-        var numElements = words.count
-        
-        for let i = 0; i < numElements; i++ {
+        let numElements = words.count
+    
+        for i in 0..<numElements {
             if words[i] != reversedWords[i] {
                 return false
             }
         }
+        return true //: must return something after the **for loop**
     }
 //: ### Why does the compiler dislike the **for loop**? Fix it.
 //: ### What else is wrong with this function? You may have to refer to (but **not**
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: [### The compiler dislikes the **for loop** because it wasn't in the right format. That's not how you initialize a **for loop**; already debugged.
+//:  ### There is no return statement after the for loop. If all of the words in *words* equal the words in *reversedWords*, then the function must return true (it did not before; debugged it). Also, at the bottom arePalindromes() is called as a type/class method so there must be a class preceding the func.]
 
 
 //: ## Q3: More Functions and Object Initialization
-    class func isAnagram() -> Bool {
-        var countLetters : [Character : Int] //Line X
-        var lenA = self.wordA.characters.count
-        var lenB = self.wordB.characters.count
+    func isAnagram() -> Bool {
+        var countLetters = [Character : Int]() //Line X
+        let lenA = self.wordA.characters.count
+        let lenB = self.wordB.characters.count
         
         if lenA != lenB {
             return false
@@ -75,13 +77,13 @@ class Words {
             }
         }
         
-        for (letter, count) in countLetters {
+        for (_, count) in countLetters {
             if count != 0 {
                 return false
             }
         }
         
-        return nil
+        return true
     }
 //: ### What is the problem with declaring **countLetters** as we do in **Line X**,
 //: ### and then using it in **Line Y**? Fix it (by only changing **Line X**).
@@ -89,7 +91,8 @@ class Words {
 //: ### change) the code at the very bottom. Debug the function.
 
 
-//: [EXPLAIN YOUR ANSWER HERE]
+//: [### It is not the correct way to declaring a dictionary. You have to assign (=) the var(iable) countLetters instead of the colon, and you need the open and closed parenthesis at the end of the initialization.
+//:  ### In the bottom, the **wordsObj** is initialized, and then the method isAnagram() is called like an instance method so isAnagram() should be an instance method, not a type/class method. Also at the end of the isAnagram() method should return true instead of nil (which is not a boolean type). ]
     
     
 }
